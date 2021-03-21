a874ca1319819393cedd175017a8195301d90800
felipe branco <Felipe.branco9@gmail.com>
2021-03-21 18:56:35 -0300

    Revert "projeto com api da marvel, sem receber informação da api"
    This reverts commit dc2f952f98c4799ce0cadeea9be6c46c048a3b5b.

--- app/src/services/api.js
+++
@@ -1,9 +0,0 @@
-const timeStamp = '1616359383';
-const apiKey = '33a2e389da8ef7446287d0caf9f217d0';
-const md5 = '7f2a027861f161291b3b68caf5405b19';
-fetch(`http://gateway.marvel.com:443/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}=${md5}&limit=100`).then((response) =>{
-  return response.json():
-  }).then((jsonParsed) =>{
-    
-  })
-export default api;
